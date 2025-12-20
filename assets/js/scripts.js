1878f7e8dc49ed5d2f3be6089158737e153e149b
Danilson Jorge Tavares Sanches <dsanches06@outlook.com>
2025-12-20 02:08:06 +0000

    feat: Menu Mobile & Responsividade

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+/* script para menu responsivo */
+const btn = document.querySelector(".menu-toggle");
+const menu = document.querySelector("nav ul");
+
+// Ao clicar no botão, liga/desliga a classe 'aberto'
+btn.addEventListener("click", () => {
+    menu.classList.toggle("aberto");
+});
