a185fce47aef84dc0fb6c5c777429d4dc5ae5571
Danilson Jorge Tavares Sanches <dsanches06@outlook.com>
2025-12-20 04:43:24 +0000

    feat: Implementar Header e Nav para ecra maior que 768px e posicionar o  carousel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,23 @@
 /* script para menu responsivo */
 const btn = document.querySelector(".menu-toggle");
 const menu = document.querySelector("nav ul");
+const carousel = document.querySelector(".carousel");
 
 // Ao clicar no botão, liga/desliga a classe 'aberto'
 btn.addEventListener("click", () => {
-    menu.classList.toggle("aberto");
+    addClassList(menu);
+    toogleSlideMenu(menu);
 });
+
+function addClassList(element) {
+    element.classList.toggle("aberto");
+}
+
+function toogleSlideMenu(element) {
+    if (element.classList.contains('aberto')) {
+        carousel.style.padding = "7rem 1rem";
+    }
+    else {
+        carousel.style.padding = "1rem";
+    }
+}
